7514a193c10f5b0021a44aca0f82415bd6a06626
Ivar Hammerset <ivar@ursolutions.no>
2021-05-12 23:06:46 +0200

    Added logo

--- src/modules/RickRollRoll.tsx
+++ src/modules/RickRollRoll.tsx
@@ -3,6 +3,8 @@ import Rickroll from "./RickRoll";
 import styled from "styled-components";
 import React, { useCallback, useEffect, useState } from "react";
 
+import superMario from "../assets/images/super-mario.png";
+
 const Layout = styled.div`
   display: grid;
   grid-template-columns: auto auto;
@@ -12,11 +14,24 @@ const Layout = styled.div`
 `;
 
 const ClickMe = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 50vw;
+
+  align-items: center;
+  padding: 5em;
+  overflow: hidden;
+
   font-size: 36px;
 
   &:hover {
-    font-size: 48px;
+    transform: scale(1.2);
     cursor: pointer;
+  }
+
+  img {
+    height: 100%;
+    width: 100%;
   }
 `;
 
@@ -49,6 +64,7 @@ const RickRollRoll: React.FC<RickRollRollProps> = ({ muted, setMuted }) => {
     <>
       {roll.length === 0 && (
         <ClickMe onClick={() => addRoll()}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Mario_Series_Logo.svg/1280px-Mario_Series_Logo.svg.png" />
           Klikk her for å starte spillet!
         </ClickMe>
       )}
